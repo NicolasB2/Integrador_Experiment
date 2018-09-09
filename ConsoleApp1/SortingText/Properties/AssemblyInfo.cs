aa8095c3471c72b827d0854cd49ae02df999d62d
Nicolas <Nicolas@BLUESKY>
2018-09-09 09:42:17 -0500

    Add QuickSort method

---
+++ ConsoleApp1/SortingText/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SortingText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("SortingText")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d424200-841e-412b-96db-6cae042eedf0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleApp1/Sorts/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Sorts
+{
+    class Program
+    {
+        public static int partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+
+                if (arr[j] <= pivot)
+                {
+                    i++;
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            // swap arr[i+1] and arr[high] (or pivot) 
+            int temp1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp1;
+
+            return i + 1;
+        }
+
+        public static void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = partition(arr, low, high);
+
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            int n = arr.Length;
+            Stopwatch t = Stopwatch.StartNew();
+            quickSort(arr, 0, n - 1);
+            Console.WriteLine(t.Elapsed.TotalMilliseconds);
+            arr.ToList().ForEach(a => Console.WriteLine(a));
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
